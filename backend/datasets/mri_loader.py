22b9f2a5ed1e419b72098511fb5da0fd04c04a86
Sandhya V <143740967+Coderpic@users.noreply.github.com>
2026-06-07 22:26:48 +0530

    Refactored MRI loader in backend

--- backend/datasets/mri_loader.py
+++ backend/datasets/mri_loader.py
@@ -1,27 +1,41 @@
 from pathlib import Path
 import h5py
 
+from mri_bbox_generator import mask_to_bbox
+
+
 def load_sample(mat_path):
-    with h5py.File(mat_path,"r") as f:
-        cjdata = f['cjdata']
-        image  = cjdata['image'][()]
-        mask = cjdata['tumorMask'][()]
-        label = int(cjdata['label'][()][0][0])
-        pid = cjdata['PID'][()]
+
+    with h5py.File(mat_path, "r") as f:
+
+        cjdata = f["cjdata"]
+
+        image = cjdata["image"][()]
+        mask = cjdata["tumorMask"][()]
+        label = int(cjdata["label"][()][0][0])
+        pid = cjdata["PID"][()]
+
+    bbox = mask_to_bbox(mask)
 
     return {
-        "image" : image,
-        "mask" : mask,
-        "label" : label,
-        "pid" : pid
+    "image": image,
+    "mask": mask,
+    "boxes": [list(bbox)],
+    "labels": [label],
+    "domain": "MRI",
+    "pid": pid,
     }
 
-def get_all_mat_files(data_dir = "data"):
+
+def get_all_mat_files(data_dir="data"):
 
     data_dir = Path(data_dir)
+
     mat_files = []
+
     for file in data_dir.rglob("*.mat"):
+
         if file.name != "cvind.mat":
             mat_files.append(file)
-    
+
     return sorted(mat_files)
